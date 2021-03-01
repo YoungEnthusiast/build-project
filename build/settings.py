25e35b067969dea504b7095ee23074356095be7e
Abe <yustaoab@gmail.com>
2021-03-02 00:18:05 +0100

    some commit

--- build/settings.py
+++ build/settings.py
@@ -104,8 +104,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'build_db',
-        'USER': 'me',
-        'PASSWORD': 'Quayers8898',
+        'USER': 'Abe',
+        'PASSWORD': ''Quayers8898'',
         'HOST': 'localhost',
         'PORT': '',
     }
