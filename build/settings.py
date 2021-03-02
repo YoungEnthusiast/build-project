91bc6445883bf1df2fb5216f6674537a71c6f432
Abe <yustaoab@gmail.com>
2021-03-02 09:17:27 +0100

    Updated

--- build/settings.py
+++ build/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'cs@wku0df^c*@#rmw-8sbm(w!i75bp&qo$@wwffnn@#px-%b61'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['http://165.232.142.35/:8000', 'http://127.0.0.1:8000/', 'www.buildqwik.ng', 'buildqwik.ng', '127.0.0.1']
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['http://165.232.142.35/:8000', 'http://127.0.0.1:8000/', 'www.buildqwik.ng', 'buildqwik.ng', '127.0.0.1']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
