9abb84b92e1a94683af070908f48d7a5e811fbe0
Yus <yustaoab@gmail.com>
2021-03-15 11:43:33 +0100

    up

--- home/filters.py
+++ home/filters.py
@@ -3,12 +3,9 @@ from django_filters import CharFilter
 from .models import Contact
 
 class ContactFilter(django_filters.FilterSet):
-    phone_number = CharFilter(field_name='phone_number', lookup_expr='icontains')
+    name = CharFilter(field_name='name', lookup_expr='icontains')
     #phone_number = CharFilter(field_name='phone_number', lookup_expr='iexact')
 
     class Meta:
         model = Contact
-        fields = ['email', 'phone_number', 'status']
-        labels = {
-            'phone_number': "Phone Number Contains",
-        }
+        fields = ['email', 'name', 'status']
