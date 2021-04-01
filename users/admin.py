80dedec7ed629f5866c13c29cf0d01cbb83dff65
Yus <yustaoab@gmail.com>
2021-04-01 17:51:07 +0100

    up

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Customer, WalletHistory
+from .models import Customer#, WalletHistory
 from django.contrib.auth.models import User
 
 class CustomerAdmin(admin.ModelAdmin):
@@ -11,12 +11,12 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 admin.site.register(Customer, CustomerAdmin)
-
-class WalletHistoryAdmin(admin.ModelAdmin):
-    list_display = ['user', 'amount_debited', 'amount_credited', 'current_balance', 'date_recorded']
-    search_fields = ['user__username', 'amount_debited', 'amount_credited', 'current']
-    list_filter = ['amount_debited', 'amount_credited']
-    list_display_links = ['user']
-    list_per_page = 10
-
-admin.site.register(WalletHistory, WalletHistoryAdmin)
+#
+# class WalletHistoryAdmin(admin.ModelAdmin):
+#     list_display = ['user', 'amount_debited', 'amount_credited', 'current_balance', 'date_recorded']
+#     search_fields = ['user__username', 'amount_debited', 'amount_credited', 'current']
+#     list_filter = ['amount_debited', 'amount_credited']
+#     list_display_links = ['user']
+#     list_per_page = 10
+#
+# admin.site.register(WalletHistory, WalletHistoryAdmin)
--- users/models.py
+++ users/models.py
@@ -20,26 +20,26 @@ class Customer(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.pk})
 
-class WalletHistory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    amount_debited = models.DecimalField(blank=True, null=True, max_digits=15, decimal_places=2)
-    amount_credited = models.DecimalField(blank=True, null=True, max_digits=15, decimal_places=2)
-    current = models.DecimalField(blank=True, null=True, max_digits=15, decimal_places=2)
-    date_recorded = models.DateTimeField(auto_now_add=True, null=True)
-    last_modified = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.user.username)
-
-    class Meta:
-        ordering = ('-date_recorded',)
-
-    @property
-    def current_balance(self):
-        if self.current == None:
-            cur = self.amount_credited
-        elif self.amount_credited == None:
-            cur = self.current
-        else:
-            cur = self.current + self.amount_credited
-        return cur
+# class WalletHistory(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+#     amount_debited = models.DecimalField(blank=True, null=True, max_digits=15, decimal_places=2)
+#     amount_credited = models.DecimalField(blank=True, null=True, max_digits=15, decimal_places=2)
+#     current = models.DecimalField(blank=True, null=True, max_digits=15, decimal_places=2)
+#     date_recorded = models.DateTimeField(auto_now_add=True, null=True)
+#     last_modified = models.DateTimeField(auto_now=True)
+#
+#     def __str__(self):
+#         return str(self.user.username)
+#
+#     class Meta:
+#         ordering = ('-date_recorded',)
+#
+#     @property
+#     def current_balance(self):
+#         if self.current == None:
+#             cur = self.amount_credited
+#         elif self.amount_credited == None:
+#             cur = self.current
+#         else:
+#             cur = self.current + self.amount_credited
+#         return cur
